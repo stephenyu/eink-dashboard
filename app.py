f75837143056fdcfae736b63da75910d019876a3
Stephen <s.yu@less3.com>
2020-05-09 16:58:47 +1000

    Update ignore bmp

--- app.py
+++ app.py
@@ -5,17 +5,19 @@ import pytz
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--dev', '-d', required=False, action='store_true', help="Run Locally i.e. not on the raspberry pi")
+parser.add_argument('--prod', '-p', required=False, action='store_true', help="Run Locally i.e. not on the raspberry pi")
 args = parser.parse_args()
 
-if args.dev:
+if args.prod:
     from inky import InkyWHAT
     inky_display = InkyWHAT("black")
     display_width = inky_display.WIDTH
     display_height = inky_display.HEIGHT
+    font_black = inky_display.BLACK
 else:
     display_width = 400
     display_height = 300
+    font_black = (0,0,0)
 
 def coord(value):
     return 4 + value
@@ -31,10 +33,10 @@ sydney_time = datetime.now(sydney_tz).strftime('%H:%M')
 uk_tz = pytz.timezone('Europe/London')
 london_time = datetime.now(uk_tz).strftime('%H:%M')
 
-draw.text((coord(0), coord(0)), "Sydney: " + sydney_time, fill=(0, 0, 0), font=font)
-draw.text((coord(0), coord(18)), "London: " + london_time, fill=(0, 0, 0), font=font)
+draw.text((coord(0), coord(0)), "Sydney: " + sydney_time, font_black, font=font)
+draw.text((coord(0), coord(18)), "London: " + london_time, font_black, font=font)
 
-if args.dev:
+if args.prod:
    inky_display.set_image(img)
    inky_display.show()
 else:
