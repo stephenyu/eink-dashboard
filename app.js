c26535e38040dec73979f8a0283be0b415de3783
stephenyu <stephen@canva.com>
2020-08-19 17:14:28 +1000

    Arguments

--- app.js
+++ app.js
@@ -4,8 +4,18 @@ const nodeHtmlToImage = require('node-html-to-image');
 const html = fs.readFileSync('./index.html', 'utf8');
 
 nodeHtmlToImage({
-  html,
-  output: './build/image.png',
+    html,
+    output: './build/image.png',
+    puppeteerArgs: {
+        args: [
+            // Required for Docker version of Puppeteer
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            // This will write shared memory files into /tmp instead of /dev/shm,
+            // because Docker’s default for /dev/shm is 64MB
+            '--disable-dev-shm-usage'
+        ]
+    }
 })
 .then(() => console.log('The image was created successfully!'))
 .catch(error => console.log(error))
