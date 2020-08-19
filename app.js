6c5a14a11ea67f74a3c57b34830ae5d972ce9581
stephenyu <stephen@canva.com>
2020-08-19 17:26:28 +1000

    Changing Tactic

--- app.js
+++ app.js
@@ -1,21 +1,43 @@
-const fs = require('fs');
-const nodeHtmlToImage = require('node-html-to-image');
+const puppeteer = require('puppeteer');
 
-const html = fs.readFileSync('./index.html', 'utf8');
+(async () => {
+const browser = await puppeteer.launch({
+    args: [
+      // Required for Docker version of Puppeteer
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      // This will write shared memory files into /tmp instead of /dev/shm,
+      // because Docker’s default for /dev/shm is 64MB
+      '--disable-dev-shm-usage'
+    ]
+  });
 
-nodeHtmlToImage({
-    html,
-    output: './build/image.png',
-    puppeteerArgs: {
-        args: [
-            // Required for Docker version of Puppeteer
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-            // This will write shared memory files into /tmp instead of /dev/shm,
-            // because Docker’s default for /dev/shm is 64MB
-            '--disable-dev-shm-usage'
-        ]
-    }
-})
-.then(() => console.log('The image was created successfully!'))
-.catch(error => console.log(error))
+const page = await browser.newPage();
+await page.goto('https://m.cnn.com/');
+await page.screenshot({ path: `./build/puppeteer.png` });
+await browser.close();
+})();
+
+
+//
+// const fs = require('fs');
+// const nodeHtmlToImage = require('node-html-to-image');
+
+// const html = fs.readFileSync('./index.html', 'utf8');
+
+// nodeHtmlToImage({
+//     html,
+//     output: './build/image.png',
+//     puppeteerArgs: {
+//         args: [
+//             // Required for Docker version of Puppeteer
+//             '--no-sandbox',
+//             '--disable-setuid-sandbox',
+//             // This will write shared memory files into /tmp instead of /dev/shm,
+//             // because Docker’s default for /dev/shm is 64MB
+//             '--disable-dev-shm-usage'
+//         ]
+//     }
+// })
+// .then(() => console.log('The image was created successfully!'))
+// .catch(error => console.log(error))
