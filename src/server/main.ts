2a4a3da79a3a7eadbb1ed8f7ab8df6b862069242
stephenyu <stephen@canva.com>
2020-09-03 08:39:01 +1000

    Init

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,3 +1,5 @@
+import * as https from 'https';
+import * as http from 'http';
 import { DashboardProps } from 'shared/types';
 
 const timezones = [
@@ -6,7 +8,47 @@ const timezones = [
   { timezone: 'Europe/London', label: 'London' },
 ];
 
+const weatherLocation = "Glebe,Sydney";
+
+async function getWeather(location: string) {
+  const wttrData = await getWeatherData(location);
+  const currentHour = new Date().getHours();
+
+  const structureWttrData = wttrData.weather.reduce<Record<string, WttrHourlyWeather[]>>((weatherCollection, daily) =>
+    ({ ...weatherCollection, [daily.date]: daily.hourly.map(hourly => hourly) }), {});
+
+}
+
+interface WttrHourlyWeather {
+  FeelsLikeC: string;
+  tempC: string;
+  time: string;
+}
+
+interface WttrDaily {
+  date: string;
+  hourly: WttrHourlyWeather[];
+}
+
+interface WttrWeather {
+  weather: WttrDaily[]
+}
+
+async function getWeatherData(location: string): Promise<WttrWeather> {
+  return new Promise(resolve => {
+    const callback = (response: http.IncomingMessage) => {
+      let body = "";
+      response.on('data', c => body += c);
+      response.on('end', () => resolve(JSON.parse(body)));
+    };
+
+    https.get('https://wttr.in/Glebe,Sydney?format=j1', callback);
+  });
+}
+
 export async function getDashboardData(): Promise<DashboardProps> {
+  const weatherData = await getWeather(weatherLocation);
+
   return Promise.resolve(
     {
       timezones,
